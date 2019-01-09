7a6d366903cebcff466f23ed7a179d97262c3e3d
곤홍 천 <take91@naver.com>
2019-01-09 16:57:25 +0900

    ..

---
+++ Ex21/Ex21/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ex21
+{
+    public delegate string FirsDelegate
+    class Program
+    {
+        string name;
+
+        static void Main(string[] args)
+        {
+            FirstDelegate d1 = new FirstDelegate();
+
+            DelegateTest instance = new DekegateTest();
+            instance.name = "My instance";
+        }
+    }
+}
